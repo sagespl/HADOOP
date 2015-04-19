34c393f3f9523060a8db13918511741491a3578f
Jakub Nowacki <j.s.nowacki@gmail.com>
2015-04-19 22:20:17 +0200

    WordCountTest added.

---
+++ mapreduce/src/test/java/pl/com/sages/hadoop/mapreduce/WordCountTest.java
@@ -0,0 +1,70 @@
+package pl.com.sages.hadoop.mapreduce;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordCountTest {
+    private MapDriver<LongWritable, Text, Text, IntWritable> mapDriver;
+    private ReduceDriver<Text, IntWritable, Text, IntWritable> reduceDriver;
+    private MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, IntWritable> mapReduceDriver;
+    String text = "Some sentence with cat.\n Another cat.";
+
+    @Before
+    public void setUp() throws Exception {
+        WordCount.Map mapper = new WordCount.Map();
+        WordCount.Reduce reducer = new WordCount.Reduce();
+        mapDriver = MapDriver.newMapDriver(mapper);
+        reduceDriver = ReduceDriver.newReduceDriver(reducer);
+        mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
+    }
+
+    @Test
+    public void testMapper() throws IOException {
+        mapDriver.withInput(new LongWritable(), new Text(text));
+        mapDriver.withOutput(new Text("Some"), new IntWritable(1));
+        mapDriver.withOutput(new Text("sentence"), new IntWritable(1));
+        mapDriver.withOutput(new Text("with"), new IntWritable(1));
+        mapDriver.withOutput(new Text("cat."), new IntWritable(1));
+        mapDriver.withOutput(new Text("Another"), new IntWritable(1));
+        mapDriver.withOutput(new Text("cat."), new IntWritable(1));
+        mapDriver.runTest();
+    }
+
+    @Test
+    public void testReducer () throws IOException {
+        Text key = new Text("lala");
+        List<IntWritable> values = new ArrayList<>();
+        IntWritable sum = new IntWritable(6);
+        for (int i=0; i < sum.get(); i++) {
+            values.add(new IntWritable(1));
+        }
+        reduceDriver.withInput(key, values);
+        reduceDriver.withOutput(key, sum);
+        reduceDriver.runTest();
+    }
+
+    @Test
+    public void testMapReduce () throws IOException {
+        LongWritable inKey = new LongWritable(0);
+        Text inValue = new Text(text);
+
+        mapReduceDriver.withInput(inKey, inValue);
+        mapReduceDriver.withOutput(new Text("Another"), new IntWritable(1));
+        mapReduceDriver.withOutput(new Text("Some"), new IntWritable(1));
+        mapReduceDriver.withOutput(new Text("cat."), new IntWritable(2));
+        mapReduceDriver.withOutput(new Text("sentence"), new IntWritable(1));
+        mapReduceDriver.withOutput(new Text("with"), new IntWritable(1));
+        mapReduceDriver.runTest();
+    }
+
+}
