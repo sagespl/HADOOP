8d1fc7b96ef31b1cb6e77470ad7cf28f5282c36b
Radosław Szmit <radoslawszmit@gmail.com>
2015-08-27 20:10:36 +0200

    Mapreduce - inverted index and wordcount

--- mapreduce/src/test/java/pl/com/sages/hadoop/mapreduce/wordcount/WordCountRunnerTest.java
+++ mapreduce/src/test/java/pl/com/sages/hadoop/mapreduce/wordcount/WordCountRunnerTest.java
@@ -1,33 +1,57 @@
 package pl.com.sages.hadoop.mapreduce.wordcount;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import pl.com.sages.hadoop.mapreduce.PropertyReader;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.junit.Assert.assertTrue;
 
 public class WordCountRunnerTest {
 
-    private final String INPUT_PATH = PropertyReader.readProperty("pl.com.sages.hadoop.mapreduce.wordcount.input");
-    private final String OUTPUT_PATH = PropertyReader.readProperty("pl.com.sages.hadoop.mapreduce.wordcount.output");
+    @Rule
+    public TemporaryFolder inputFolder = new TemporaryFolder();
+
+    @Rule
+    public TemporaryFolder outputFolder = new TemporaryFolder();
+
+    private String inputPath;
+    private String outputPath;
+
+    @Before
+    public void before() throws IOException {
+        File tmpFile = inputFolder.newFile();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("20-000-mil-podmorskiej-zeglugi.txt");
+        IOUtils.copy(inputStream, new FileOutputStream(tmpFile));
+
+        inputPath = inputFolder.getRoot().getAbsolutePath();
+        outputPath = outputFolder.getRoot().getAbsolutePath();
+    }
 
     @Test
     public void shouldCountWords() throws Exception {
         //given
         FileSystem fs = FileSystem.get(new Configuration());
-        fs.delete(new Path(OUTPUT_PATH), true);
+        fs.delete(new Path(outputPath), true);
 
-        Job job = WordCountRunner.createJob(new Path(INPUT_PATH), new Path(OUTPUT_PATH));
+        Job job = WordCountRunner.createJob(new Path(inputPath), new Path(outputPath));
 
         //when
         job.waitForCompletion(true);
 
         //then
-        assertTrue(fs.exists(new Path(OUTPUT_PATH + "/_SUCCESS")));
-        assertTrue(fs.exists(new Path(OUTPUT_PATH + "/part-r-00000")));
+        assertTrue(fs.exists(new Path(outputPath + "/_SUCCESS")));
+        assertTrue(fs.exists(new Path(outputPath + "/part-r-00000")));
     }
 
 }
