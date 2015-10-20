c42c4926295573f8dcb810fa7a04f83aaeeb5c00
Radosław Szmit <radoslawszmit@gmail.com>
2015-10-21 01:48:52 +0200

    HBase - get all versioned data for row

--- hbase/hbase-api/src/test/java/pl/com/sages/hbase/api/HbaseApiExternalTest.java
+++ hbase/hbase-api/src/test/java/pl/com/sages/hbase/api/HbaseApiExternalTest.java
@@ -1,15 +1,16 @@
 package pl.com.sages.hbase.api;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static pl.com.sages.hbase.api.conf.HbaseConfigurationFactory.getConfiguration;
 
 public class HbaseApiExternalTest {
@@ -29,7 +30,7 @@ public class HbaseApiExternalTest {
         HTableDescriptor table = new HTableDescriptor(TEST_TABLE_NAME);
 
         HColumnDescriptor columnFamily = new HColumnDescriptor(TEST_FAMILY_NAME);
-        columnFamily.setMaxVersions(1);
+        columnFamily.setMaxVersions(10);
         table.addFamily(columnFamily);
 
         admin.createTable(table);
@@ -60,6 +61,41 @@ public class HbaseApiExternalTest {
         users.put(put);
 
         //then
+    }
+
+    @Test
+    public void shouldGetDataFromHbase() throws Exception {
+        //given
+        Table users = connection.getTable(TEST_TABLE_NAME);
+
+        String id = "id";
+        String column = "cell";
+        String value1 = "nasza testowa wartość";
+        String value2 = "nasza testowa wartość 2";
+
+        Put put = new Put(Bytes.toBytes(id));
+        put.addColumn(Bytes.toBytes(TEST_FAMILY_NAME),
+                Bytes.toBytes(column),
+                Bytes.toBytes(value1));
+        users.put(put);
+
+        put = new Put(Bytes.toBytes(id));
+        put.addColumn(Bytes.toBytes(TEST_FAMILY_NAME),
+                Bytes.toBytes(column),
+                Bytes.toBytes(value2));
+        users.put(put);
+
+        //when
+        Get get = new Get(Bytes.toBytes(id));
+        get.setMaxVersions(10);
+        Result result = users.get(get);
+
+        //then
+        assertThat(value2).isEqualToIgnoringCase(Bytes.toString(result.getValue(Bytes.toBytes(TEST_FAMILY_NAME), Bytes.toBytes(column))));
+
+        List<Cell> columnCells = result.getColumnCells(Bytes.toBytes(TEST_FAMILY_NAME), Bytes.toBytes(column));
+        assertThat(value2).isEqualToIgnoringCase(Bytes.toString(CellUtil.cloneValue(columnCells.get(0))));
+        assertThat(value1).isEqualToIgnoringCase(Bytes.toString(CellUtil.cloneValue(columnCells.get(1))));
     }
 
 }
