62e4cb519c7a04d80236184cad15d8acbfecd3e3
Radosław Szmit <radoslawszmit@gmail.com>
2017-02-04 00:29:30 +0100

    Kafka

---
+++ kafka/src/main/scala/pl/com/sages/hadoop/kafka/KafkaProducerScalaExample.scala
@@ -0,0 +1,41 @@
+package pl.com.sages.hadoop.kafka
+
+import java.util.{Date, Properties}
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+
+/**
+  * Created by radek on 04.02.17.
+  */
+object KafkaProducerScalaExample {
+
+  def main(args: Array[String]): Unit = {
+
+    val props: Properties = new Properties
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    props.put(ProducerConfig.ACKS_CONFIG, "all")
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+
+    while (true) {
+
+      val producer = new KafkaProducer[String, String](props)
+
+      for (i <- 1 to 10) {
+
+        val data: ProducerRecord[String, String] = new ProducerRecord[String, String]("my-partitioned-topic", "key-" + i, "message-" + i)
+        producer.send(data)
+
+      }
+
+      producer.flush()
+      producer.close()
+
+      println("Sended messages")
+
+      Thread.sleep(5000)
+    }
+
+  }
+
+}
---
+++ kafka/src/main/scala/pl/com/sages/hadoop/kafka/Person.scala
@@ -0,0 +1,15 @@
+package pl.com.sages.hadoop.kafka
+
+/**
+  * Created by radek on 30.01.17.
+  */
+object Person {
+
+//  val name: String
+//  val age: Int
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
