25d265427b104bed8ed1c03fdeb8e5a2ed2f7ff5
Radosław Szmit <radoslawszmit@gmail.com>
2017-02-04 02:49:36 +0100

    Scala

---
+++ kafka/src/main/scala/pl/com/sages/hadoop/scalatutorial/ApplicationInScala.scala
@@ -0,0 +1,20 @@
+package pl.com.sages.hadoop.scalatutorial
+
+import scala.io.Source
+
+/**
+  * Created by radek on 04.02.17.
+  */
+object ApplicationInScala extends App {
+
+  println("To jest wnętrz naszej aplikacji")
+
+  val source = Source.fromFile("/home/radek/Sages/repository/HADOOP/mapreduce/src/test/resources/20-000-mil-podmorskiej-zeglugi.txt", "UTF-8")
+
+  val lineIterator = source.getLines
+
+  val lines = source.getLines.toArray
+
+  for (line <- lines) println(line)
+
+}
