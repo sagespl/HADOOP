29cc8f0275c0f217448d29dfa72e6f249756f438
Radosław Szmit <radoslawszmit@gmail.com>
2017-05-13 02:03:35 +0200

    HBase MR

---
+++ hbase/hbase-mapred/README.md
@@ -0,0 +1,6 @@
+
+# Instrukcja uruchomienia projektu na klastrze Apache Hadoop
+
+```
+yarn jar $HADOOP_PROJECT/hbase/hbase-mapred/target/hbase-mapred-1.0-SNAPSHOT-job.jar pl.com.sages.hbase.mapred.movies.AverageRatingToTableRunner
+```
---
+++ hbase/hbase-mapred/src/main/java/pl/com/sages/hbase/mapred/movies/AverageRatingToTableRunner.java
@@ -0,0 +1,45 @@
+package pl.com.sages.hbase.mapred.movies;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import pl.com.sages.hbase.api.dao.RatingDao;
+
+import static pl.com.sages.hbase.mapred.movies.MovieAverageRatingsConstants.*;
+
+public class AverageRatingToTableRunner {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set(AverageRatingReducer.FAMILY, FAMILY_NAME);
+        configuration.set(AverageRatingReducer.QUALIFIER, QUALIFIER_NAME);
+
+        Job job = Job.getInstance(configuration, "Average Rating");
+        job.setJarByClass(AverageRatingMapper.class);
+
+        Scan scan = new Scan();
+        scan.setCaching(500);
+        scan.setCacheBlocks(false);
+        scan.addFamily(RatingDao.CF);
+
+        TableMapReduceUtil.initTableMapperJob(
+                RatingDao.TABLE,
+                scan,
+                AverageRatingMapper.class,
+                IntWritable.class,
+                DoubleWritable.class,
+                job);
+        TableMapReduceUtil.initTableReducerJob(
+                TABLE_NAME.getNameAsString(),
+                AverageRatingReducer.class,
+                job);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
