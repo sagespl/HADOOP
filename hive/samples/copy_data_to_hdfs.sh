e032863c8f22567fb93fc1b43e271180dcf7c1fb
Radosław Szmit <radoslawszmit@gmail.com>
2017-09-14 00:00:26 +0200

    Poprawki

--- hive/samples/copy_data_to_hdfs.sh
+++ hive/samples/copy_data_to_hdfs.sh
@@ -1,5 +1,15 @@
 
-hdfs dfs -copyFromLocal -f /home/sages/dane/movie /user/sages/dane
+cd $HADOOP_DATA
+rm -rf movie
+cp -r ml-10M100K movie
+
+cd movie
+sed -i -e 's/::/@/g' movies.dat
+sed -i -e 's/::/@/g' ratings.dat
+sed -i -e 's/::/@/g' tags.dat
+
+hdfs dfs -rm -r -skipTrash /user/sages/dane/movie
+hdfs dfs -mkdir /user/sages/dane/movie
 
 hdfs dfs -mkdir /user/sages/dane/movie/movies/
 hdfs dfs -copyFromLocal -f /home/sages/dane/movie/movies.dat /user/sages/dane/movie/movies/
--- hive/samples/create_tables.sql
+++ hive/samples/create_tables.sql
@@ -1,6 +1,15 @@
+
+set hive.cli.print.current.db=true;
+
+-- stworzenie bazy danych
+DROP database if exists sages;
+CREATE database sages;
 use sages;
 
 
+
+
+-- tabela filmów
 drop table if exists movies;
 
 CREATE TABLE IF NOT EXISTS movies (
@@ -12,27 +21,27 @@ ROW FORMAT DELIMITED FIELDS TERMINATED BY '@';
 LOAD DATA LOCAL INPATH '/home/sages/dane/movie/movies.dat'
 OVERWRITE INTO TABLE movies;
 
-
+-- tabela ocen
 drop table if exists ratings;
 
 CREATE TABLE IF NOT EXISTS ratings (
 UserID STRING,
 MovieID STRING,
 Rating STRING,
-Timestamp STRING)
+Time STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '@';
 
 LOAD DATA LOCAL INPATH '/home/sages/dane/movie/ratings.dat'
 OVERWRITE INTO TABLE ratings;
 
-
+-- tabela takgów
 drop table if exists tags;
 
 CREATE TABLE IF NOT EXISTS tags (
 UserID STRING,
 MovieID STRING,
 Tag STRING,
-Timestamp STRING)
+Time STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '@';
 
 LOAD DATA LOCAL INPATH '/home/sages/dane/movie/tags.dat'
