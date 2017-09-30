2eb54eac9a1fe012a5c8590746dfd4286c1b3efb
Radosław Szmit <radoslawszmit@gmail.com>
2017-09-30 23:00:00 +0200

    Apache HBase

--- hive/samples/movielens/copy_data_to_hdfs.sh
+++ hive/samples/movielens/copy_data_to_hdfs.sh
@@ -8,16 +8,16 @@ sed -i -e 's/::/@/g' movies.dat
 sed -i -e 's/::/@/g' ratings.dat
 sed -i -e 's/::/@/g' tags.dat
 
-hdfs dfs -rm -r -skipTrash /user/sages/dane/movie
-hdfs dfs -mkdir /user/sages/dane/movie
+hdfs dfs -rm -r -skipTrash /user/`whoami`/dane/movie
+hdfs dfs -mkdir /user/`whoami`/dane/movie
 
-hdfs dfs -mkdir /user/sages/dane/movie/movies/
-hdfs dfs -copyFromLocal -f /home/sages/dane/movie/movies.dat /user/sages/dane/movie/movies/
+hdfs dfs -mkdir /user/`whoami`/dane/movie/movies/
+hdfs dfs -copyFromLocal -f /home/`whoami`/dane/movielens/hive/movies.dat /user/`whoami`/dane/movie/movies/
 
-hdfs dfs -mkdir /user/sages/dane/movie/ratings/
-hdfs dfs -copyFromLocal -f /home/sages/dane/movie/ratings.dat /user/sages/dane/movie/ratings/
+hdfs dfs -mkdir /user/`whoami`/dane/movie/ratings/
+hdfs dfs -copyFromLocal -f /home/`whoami`/dane/movielens/hive/ratings.dat /user/`whoami`/dane/movie/ratings/
 
-hdfs dfs -mkdir /user/sages/dane/movie/tags/
-hdfs dfs -copyFromLocal -f /home/sages/dane/movie/tags.dat /user/sages/dane/movie/tags/
+hdfs dfs -mkdir /user/`whoami`/dane/movie/tags/
+hdfs dfs -copyFromLocal -f /home/`whoami`/dane/movielens/hive/tags.dat /user/`whoami`/dane/movie/tags/
 
 
