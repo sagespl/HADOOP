457a79be70f7284bc8ae73a7b0440ed777adb84a
Radosław Szmit <radoslawszmit@gmail.com>
2017-11-20 23:26:10 +0100

    HDFS + snapshot

---
+++ hdfs/src/main/java/pl/com/sages/hdfs/HdfsManager.java
@@ -0,0 +1,39 @@
+package pl.com.sages.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.hdfs.protocol.SnapshottableDirectoryStatus;
+
+import java.io.IOException;
+
+public class HdfsManager {
+
+    public static void main(String[] args) throws IOException {
+        System.setProperty("HADOOP_USER_NAME", "hdfs");
+
+        Configuration conf = new Configuration(false);
+        conf.addResource(HdfsManager.class.getClassLoader().getResourceAsStream("hdfs-configuration.xml"));
+        FileSystem fs = FileSystem.get(conf);
+
+
+        DistributedFileSystem dfs = (DistributedFileSystem) fs;
+        SnapshottableDirectoryStatus[] stats = dfs.getSnapshottableDirListing();
+        for (SnapshottableDirectoryStatus stat : stats) {
+
+            Path snapshotableDirectory = stat.getFullPath();
+            System.out.println(snapshotableDirectory);
+
+            FileStatus[] fileStatuses = dfs.listStatus(new Path(snapshotableDirectory, ".snapshot"));
+            for (FileStatus fileStatus : fileStatuses) {
+                String snapshotName = fileStatus.getPath().getName();
+                System.out.println("Usuwam snapshot: " + snapshotName);
+                dfs.deleteSnapshot(snapshotableDirectory, snapshotName);
+            }
+        }
+
+    }
+
+}
