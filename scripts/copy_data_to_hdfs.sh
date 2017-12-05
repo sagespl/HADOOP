086637cba8e76cfb0bc726b327cf8fe5ce06071a
Radosław Szmit <radoslawszmit@gmail.com>
2017-12-05 23:35:35 +0100

    MR

--- scripts/copy_data_to_hdfs.sh
+++ scripts/copy_data_to_hdfs.sh
@@ -1,8 +1,10 @@
 
+HADOOP_USER_NAME=hdfs
+
 hdfs dfs -rm -r -skipTrash /dane
 
-hdfs dfs -mkdir /dane
-hdfs dfs -cp $HADOOP_DATA/* /dane
+hdfs dfs -cp $HADOOP_DATA /
 
 hdfs dfs -ls /dane
 
+unset HADOOP_USER_NAME
