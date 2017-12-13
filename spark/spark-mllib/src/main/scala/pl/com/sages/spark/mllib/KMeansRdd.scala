9f11f270459491ea0135ad32d04f4cf58ece14b4
Radosław Szmit <radoslawszmit@gmail.com>
2017-12-13 18:13:50 +0100

    Apache Spark

--- spark/spark-mllib/src/main/scala/pl/com/sages/spark/mllib/KMeansRdd.scala
+++ spark/spark-mllib/src/main/scala/pl/com/sages/spark/mllib/KMeansRdd.scala
@@ -20,6 +20,8 @@ object KMeansRdd extends GlobalMlParameters {
     val data = sc.textFile(sparkKMeansData)
     val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
 
+    parsedData.collect().foreach(println)
+
     // Cluster the data into two classes using KMeans
     val numClusters = 2
     val numIterations = 20
@@ -34,6 +36,18 @@ object KMeansRdd extends GlobalMlParameters {
     FileUtils.deleteDirectory(new File(outputDir))
     clusters.save(sc, outputDir)
     val sameModel = KMeansModel.load(sc, outputDir)
+
+    // Shows the result.
+    println("Cluster Centers: ")
+    sameModel.clusterCenters.foreach(println)
+
+    // Prediction
+    println("Prediction: ")
+    println(sameModel.predict(Vectors.dense(0, 0, 0)))
+    println(sameModel.predict(Vectors.dense(1, 1, 1)))
+    println(sameModel.predict(Vectors.dense(2, 2, 2)))
+    println(sameModel.predict(Vectors.dense(9, 9, 9)))
+    println(sameModel.predict(Vectors.dense(10, 10, 10)))
 
     // stop
     sc.stop()
