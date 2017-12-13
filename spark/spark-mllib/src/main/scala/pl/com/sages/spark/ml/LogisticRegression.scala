7097dd91968f2e1f9653ff428e96f10eaa93e49a
Radosław Szmit <radoslawszmit@gmail.com>
2017-12-13 20:46:28 +0100

    Apache Spark

--- spark/spark-mllib/src/main/scala/pl/com/sages/spark/ml/LogisticRegression.scala
+++ spark/spark-mllib/src/main/scala/pl/com/sages/spark/ml/LogisticRegression.scala
@@ -1,7 +1,8 @@
-package pl.com.sages.spark
+package pl.com.sages.spark.ml
 
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.sql.SparkSession
+import pl.com.sages.spark.GlobalMlParameters
 
 object LogisticRegression extends GlobalMlParameters {
 
