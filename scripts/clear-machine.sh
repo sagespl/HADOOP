b833f52a53cddfa96aee4c93c33fc94e53c5cdd2
Radosław Szmit <radoslawszmit@gmail.com>
2017-12-13 21:40:10 +0100

    Apache Spark

--- scripts/clear-machine.sh
+++ scripts/clear-machine.sh
@@ -9,8 +9,13 @@ rm -rf /hadoop/yarn/local/usercache
 
 rm -rf /home/sages/.m2/repository/pl/com/sages
 
+df -h
+
 # hdfs
-# hdfs dfs -rm -r -skipTrash /user/sages/*
+su hdfs -c 'hdfs dfs -rm -r -skipTrash /user/radek'
+su hdfs -c 'hdfs dfs -rm -r -skipTrash /user/sages'
+su hdfs -c 'hdfs dfs -mkdir /user/sages'
+su hdfs -c 'hdfs dfs -chown sages /user/sages'
 
 # hive
 
