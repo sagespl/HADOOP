9e1d3a36034602f30b8b06ffbae92af881fc6789
Radosław Szmit <radoslawszmit@gmail.com>
2017-12-14 22:45:15 +0100

    Spark

--- spark/spark-sql/src/main/scala/pl/com/sages/spark/GlobalSqlParameters.scala
+++ spark/spark-sql/src/main/scala/pl/com/sages/spark/GlobalSqlParameters.scala
@@ -2,13 +2,16 @@ package pl.com.sages.spark
 
 trait GlobalSqlParameters {
 
+  // books
   val bookPath: String = "/user/sages/dane/lektury/lektury-all"
 
+  // movielens
   val moviesPath: String = "/user/sages/dane/movielens/hive/movies/movies.dat"
   val tagsPath: String = "/user/sages/dane/movielens/hive/tags/tags.dat"
   val ratingsPath: String = "/user/sages/dane/movielens/hive/ratings/ratings.dat"
   val movielensSeparator: String = "@"
 
+  // results
   val resultPath: String = "/user/sages/wyniki/spark"
 
 }
