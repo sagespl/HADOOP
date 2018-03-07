e7355580e29f711cacb5ef10fbdee3d4ab2b53b7
Radosław Szmit <radoslawszmit@gmail.com>
2018-03-07 23:57:58 +0100

    Apache Spark

--- spark/spark-core/src/main/scala/pl/com/sages/spark/GlobalParameters.scala
+++ spark/spark-core/src/main/scala/pl/com/sages/spark/GlobalParameters.scala
@@ -2,16 +2,18 @@ package pl.com.sages.spark
 
 trait GlobalParameters {
 
+  val user: String = "sages"
+
   // books
-  val bookPath: String = "/user/sages/dane/lektury/lektury-all"
+  val bookPath: String = "/dane/lektury/lektury-all"
 
   // movielens
-  val moviesPath: String = "/user/sages/dane/movielens/hive/movies/movies.dat"
-  val tagsPath: String = "/user/sages/dane/movielens/hive/tags/tags.dat"
-  val ratingsPath: String = "/user/sages/dane/movielens/hive/ratings/ratings.dat"
+  val moviesPath: String = "/dane/movielens/hive/movies/movies.dat"
+  val tagsPath: String = "/dane/movielens/hive/tags/tags.dat"
+  val ratingsPath: String = "/dane/movielens/hive/ratings/ratings.dat"
   val movielensSeparator: String = "@"
 
   // results
-  val resultPath: String = "/user/sages/wyniki/spark"
+  val resultPath: String = "/user/" + user + "/wyniki/spark"
 
 }
