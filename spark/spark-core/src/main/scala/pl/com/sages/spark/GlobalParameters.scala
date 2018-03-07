2ab91a6e56a29e19bc698cc2100e7c91ea9536c0
Radosław Szmit <radoslawszmit@gmail.com>
2018-03-08 00:17:37 +0100

    Apache Spark

--- spark/spark-core/src/main/scala/pl/com/sages/spark/GlobalParameters.scala
+++ spark/spark-core/src/main/scala/pl/com/sages/spark/GlobalParameters.scala
@@ -2,7 +2,8 @@ package pl.com.sages.spark
 
 trait GlobalParameters {
 
-  val user: String = "sages"
+  val user: String = System.getProperty("user.name")
+  val master: String = "local"
 
   // books
   val bookPath: String = "/dane/lektury/lektury-all"
@@ -12,6 +13,15 @@ trait GlobalParameters {
   val tagsPath: String = "/dane/movielens/hive/tags/tags.dat"
   val ratingsPath: String = "/dane/movielens/hive/ratings/ratings.dat"
   val movielensSeparator: String = "@"
+
+  // Spark test data
+  val sparkSampleData: String = System.getenv("HADOOP_DATA") + "/spark-data"
+  val sparkSampleLibsvmData: String = sparkSampleData + "/mllib/sample_libsvm_data.txt"
+  val sparkSampleMovielensRatingsData: String = sparkSampleData + "/mllib/als/sample_movielens_ratings.txt"
+  val sparkAlsTestData: String = sparkSampleData + "/mllib/als/test.data"
+  val sparkKMeansData: String = sparkSampleData + "/mllib/kmeans_data.txt"
+  val sparkSampleKMeansData: String = sparkSampleData + "/mllib/sample_kmeans_data.txt"
+  val sparkSampleLinearRegressionData: String = sparkSampleData + "/mllib/sample_linear_regression_data.txt"
 
   // results
   val resultPath: String = "/user/" + user + "/wyniki/spark"
