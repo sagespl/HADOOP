7c3d6753a7b056fad9f2601aa792c55421f3f156
Radosław Szmit <radoslawszmit@gmail.com>
2018-03-07 23:33:44 +0100

    Apache Spark

---
+++ spark/spark-streaming/src/main/scala/pl/com/sages/spark/SparkStreamingHdfsExample.scala
@@ -0,0 +1,29 @@
+package pl.com.sages.spark
+
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object SparkStreamingHdfsExample {
+
+  def main(args: Array[String]) {
+    if (args.length < 1) {
+      System.err.println("Usage: HdfsWordCount <directory>")
+      System.exit(1)
+    }
+
+    val sparkConf = new SparkConf().setAppName("HdfsWordCount")
+    // Create the context
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+
+    // Create the FileInputDStream on the directory and use the
+    // stream to count words in new files created
+    val lines = ssc.textFileStream(args(0))
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
+    wordCounts.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
