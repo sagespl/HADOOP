fce9ac0c73ad1baa34012d49cda66b2a9523d1dc
Radosław Szmit <radoslawszmit@gmail.com>
2018-04-15 11:05:27 +0200

    Zadanie HBase

--- hbase/hbase-api/src/main/java/pl/com/sages/hbase/api/RadekBooksLoader.java
+++ hbase/hbase-api/src/main/java/pl/com/sages/hbase/api/RadekBooksLoader.java
@@ -5,14 +5,13 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Admin;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.*;
 import pl.com.sages.hbase.api.util.HBaseUtil;
 
 import java.io.File;
 import java.io.FileInputStream;
 
+import static org.apache.hadoop.hbase.util.Bytes.toBytes;
 import static pl.com.sages.hbase.api.util.HbaseConfigurationFactory.getConfiguration;
 
 /**
@@ -24,32 +23,37 @@ import static pl.com.sages.hbase.api.util.HbaseConfigurationFactory.getConfigura
  */
 public class RadekBooksLoader {
 
-    private static final String BOOKS = "/home/users/login/bigdata/lektury/lektury-all";
-//    public static final String BOOKS = "/home/radek/Sages/dane/lektury/lektury-all";
+//    private static final String BOOKS = "/home/users/login/bigdata/lektury/lektury-all";
+    public static final String BOOKS = "/home/radek/Sages/dane/lektury/lektury-all";
 
     private static final TableName TABLE_NAME = HBaseUtil.getUserTableName("books");
     private static final String FAMILY_NAME = "cf";
 
-
     // psvm
     public static void main(String[] args) throws Exception {
         // wczytywanie lektur do bazy (lektury-all)
-        // tabelka (name, content) z zbioru lektury-all
+        // tabelka (name jako id, content) z zbioru lektury-all
         Configuration configuration = getConfiguration();
         Connection connection = ConnectionFactory.createConnection(configuration);
         Admin admin = connection.getAdmin();
 
+        // ponowne utworzenie tabeli
         if (admin.tableExists(TABLE_NAME)) {
             admin.disableTable(TABLE_NAME);
             admin.deleteTable(TABLE_NAME);
         }
         admin.createTable(new HTableDescriptor(TABLE_NAME).addFamily(new HColumnDescriptor(FAMILY_NAME)));
 
+        // ładowanie plików
+        Table table = connection.getTable(TABLE_NAME);
         File[] files = new File(BOOKS).listFiles();
         if (files != null) {
             for (File file : files) {
                 String content = IOUtils.toString(new FileInputStream(file));
-                System.out.println(content);
+
+                table.put(new Put(toBytes(file.getName()))
+                        .addColumn(toBytes(FAMILY_NAME), toBytes("content"), toBytes(content)));
+
             }
         }
     }
