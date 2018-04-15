2ddf72f4ffe09feefb97b3c37b0d864a5a37b4a7
Radosław Szmit <radoslawszmit@gmail.com>
2018-04-15 13:24:25 +0200

    Zadanie HBase

---
+++ hbase/hbase-mapred/src/main/java/pl/com/sages/hbase/mapred/search/RadekSearchRunner.java
@@ -0,0 +1,48 @@
+package pl.com.sages.hbase.mapred.search;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.mapreduce.Job;
+import pl.com.sages.hbase.api.RadekBooksLoader;
+import pl.com.sages.hbase.api.util.HBaseUtil;
+
+public class RadekSearchRunner {
+
+    public static final TableName TABLE_NAME = HBaseUtil.getUserTableName("search_index");
+    public static final String FAMILY_NAME = RadekBooksLoader.FAMILY_NAME;
+
+    public static void main(String[] args) throws Exception {
+        run();
+    }
+
+    public static boolean run() throws Exception {
+        Configuration configuration = HBaseConfiguration.create();
+        Job job = Job.getInstance(configuration, "Search index");
+        job.setJarByClass(RadekSearchMapper.class);
+
+        Scan scan = new Scan();
+        scan.setCaching(500);
+        scan.setCacheBlocks(false);
+
+        // Na wejściu tabela filmów
+        TableMapReduceUtil.initTableMapperJob(
+                RadekBooksLoader.TABLE_NAME,
+                scan,
+                RadekSearchMapper.class,
+                null,
+                null,
+                job);
+        TableMapReduceUtil.initTableReducerJob(
+                TABLE_NAME.getNameAsString(),
+                null,
+                job);
+        // Brak reduktora -> patrz implementację Mapper'a
+        job.setNumReduceTasks(0);
+
+        return job.waitForCompletion(true);
+    }
+
+}
--- hbase/hbase-mapred/src/test/java/pl/com/sages/hbase/mapred/search/RadekSearchMapperTest.java
+++ hbase/hbase-mapred/src/test/java/pl/com/sages/hbase/mapred/search/RadekSearchMapperTest.java
@@ -1,69 +1,33 @@
 package pl.com.sages.hbase.mapred.search;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Scan;
-import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
-import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.mapreduce.Job;
 import org.junit.Before;
 import org.junit.Test;
-import pl.com.sages.hbase.api.RadekBooksLoader;
-import pl.com.sages.hbase.api.dao.MovieDao;
 import pl.com.sages.hbase.api.util.ConnectionHandler;
 import pl.com.sages.hbase.api.util.HBaseTableUtil;
 import pl.com.sages.hbase.api.util.HBaseUtil;
-import pl.com.sages.hbase.mapred.filter.FilterMapper;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
 public class RadekSearchMapperTest {
 
-    private static final TableName TABLE_NAME = HBaseUtil.getUserTableName("search_index");
-    private static final String FAMILY_NAME = RadekBooksLoader.FAMILY_NAME;
-
     @Before
     public void before() throws IOException {
-        HBaseUtil.recreateTable(TABLE_NAME, FAMILY_NAME);
+        HBaseUtil.recreateTable(RadekSearchRunner.TABLE_NAME, RadekSearchRunner.FAMILY_NAME);
     }
 
     @Test
     public void shouldRunMapReduce() throws Exception {
         //given
-        Configuration configuration = HBaseConfiguration.create();
-        Job job = Job.getInstance(configuration, "Search index");
-        job.setJarByClass(FilterMapper.class);
-
-        Scan scan = new Scan();
-        scan.setCaching(500);
-        scan.setCacheBlocks(false);
-
-        // Na wejściu tabela filmów
-        TableMapReduceUtil.initTableMapperJob(
-                RadekBooksLoader.TABLE_NAME,
-                scan,
-                RadekSearchMapper.class,
-                null,
-                null,
-                job);
-        TableMapReduceUtil.initTableReducerJob(
-                TABLE_NAME.getNameAsString(),
-                null,
-                job);
-        // Brak reduktora -> patrz implementację Mapper'a
-        job.setNumReduceTasks(0);
 
         //when
-        boolean succeeded = job.waitForCompletion(true);
+        boolean succeeded = RadekSearchRunner.run();
 
         //then
         assertThat(succeeded).isTrue();
-        assertThat(ConnectionHandler.getConnection().getAdmin().tableExists(TABLE_NAME)).isTrue();
-        assertThat(HBaseTableUtil.countNumberOfRows(TABLE_NAME)).isGreaterThan(1000);
+        assertThat(ConnectionHandler.getConnection().getAdmin().tableExists(RadekSearchRunner.TABLE_NAME)).isTrue();
+        assertThat(HBaseTableUtil.countNumberOfRows(RadekSearchRunner.TABLE_NAME)).isGreaterThan(1000);
     }
 
 }
