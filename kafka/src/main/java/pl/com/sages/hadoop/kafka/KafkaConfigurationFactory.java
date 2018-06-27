84a989b2c1fb5ae606101973ef9e18778f9259df
Radosław Szmit <radoslawszmit@gmail.com>
2018-06-27 02:52:02 +0200

    Apache Kafka

--- kafka/src/main/java/pl/com/sages/hadoop/kafka/KafkaConfigurationFactory.java
+++ kafka/src/main/java/pl/com/sages/hadoop/kafka/KafkaConfigurationFactory.java
@@ -17,7 +17,7 @@ public class KafkaConfigurationFactory {
 
     public static final String ZOOKEEPER_SERVER = "cluster_zookeeper:2181";
 
-    public static final String TOPIC = "test";
+    public static final String TOPIC = "test-topic";
     public static final String TOPIC_OUT = "test-topic-out";
 
     public static final int TIMEOUT = 10000;
