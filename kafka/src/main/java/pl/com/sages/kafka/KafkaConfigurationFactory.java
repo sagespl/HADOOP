0fb9711dfbef830daf8a49114e29dc68be076e95
Radosław Szmit <radoslawszmit@gmail.com>
2018-06-30 22:52:36 +0200

    Apache Kafka

--- kafka/src/main/java/pl/com/sages/kafka/KafkaConfigurationFactory.java
+++ kafka/src/main/java/pl/com/sages/kafka/KafkaConfigurationFactory.java
@@ -43,15 +43,16 @@ public class KafkaConfigurationFactory {
     public static Properties createProducerConfig() {
         Properties producerConfig = new Properties();
         producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        // producerConfig.put(ProducerConfig.CLIENT_ID_CONFIG, "");
 
         producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
         producerConfig.put(ProducerConfig.RETRIES_CONFIG, 0);
-        producerConfig.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none");
-        producerConfig.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
-        producerConfig.put(ProducerConfig.LINGER_MS_CONFIG, "1");
+        // producerConfig.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none");
+        // producerConfig.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+        // producerConfig.put(ProducerConfig.LINGER_MS_CONFIG, "1");
 
         return producerConfig;
     }
