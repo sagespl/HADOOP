568b8237a108448d45bb96e01786ea165f3c75d4
Radosław Szmit <radoslawszmit@gmail.com>
2018-06-30 22:29:24 +0200

    Apache Kafka

--- kafka/src/main/java/pl/com/sages/kafka/KafkaConfigurationFactory.java
+++ kafka/src/main/java/pl/com/sages/kafka/KafkaConfigurationFactory.java
@@ -28,19 +28,28 @@ public class KafkaConfigurationFactory {
         Properties consumerConfig = new Properties();
         consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
         consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
-        consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
         consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getCanonicalName());
+
+        consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
         return consumerConfig;
     }
 
+    /**
+     * https://kafka.apache.org/documentation/#producerconfigs
+     */
     public static Properties createProducerConfig() {
         Properties producerConfig = new Properties();
         producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+
+        producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
+        producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
+
         producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
         producerConfig.put(ProducerConfig.RETRIES_CONFIG, 0);
-        producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
-        producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getCanonicalName());
+        producerConfig.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none");
+
         return producerConfig;
     }
 
