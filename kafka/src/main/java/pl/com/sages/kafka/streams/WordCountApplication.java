6faf248bdb9a8c473e39b8c08c723b32c80f192b
Radosław Szmit <radoslawszmit@gmail.com>
2018-07-01 11:39:48 +0200

    Kafka

--- kafka/src/main/java/pl/com/sages/kafka/streams/WordCountApplication.java
+++ kafka/src/main/java/pl/com/sages/kafka/streams/WordCountApplication.java
@@ -20,13 +20,11 @@ public class WordCountApplication {
         Properties config = getStreamConfig();
 
         KStreamBuilder builder = new KStreamBuilder();
-
         KStream<String, String> source = builder.stream(TOPIC);
-
         KStream counts = source
                 .flatMapValues(value -> Arrays.asList(PATTERN.split(value.toLowerCase())))
                 .map((key, value) -> new KeyValue<Object, Object>(value, value))
-//                .filter((key, value) -> (!value.equals("the")))
+                .filter((key, value) -> (!value.equals("ma")))
                 .groupByKey()
                 .count("CountStore")
                 .mapValues(value -> Long.toString(value))
@@ -35,14 +33,16 @@ public class WordCountApplication {
         counts.to(TOPIC_OUT);
 
         KafkaStreams streams = new KafkaStreams(builder, config);
-//        streams.cleanUp();
 
-        // start
+        // Nie używać na produkcji, czyści stan strumieni
+        // streams.cleanUp();
+
+        // start Kafka Streams
         streams.start();
 
-        // run and close
-//        Thread.sleep(5000L);
-//        streams.close();
+        // jeśli chcemy zamknać aplikację po jakimś czasie to najprościej dajemy sleep i close
+        // Thread.sleep(5000L);
+        // streams.close();
 
         // Eleganckie zamknięcie
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
