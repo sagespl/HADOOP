d686ac8bc7aa78232c8a640084a2cf8f8566dba1
Radosław Szmit <radoslawszmit@gmail.com>
2018-07-10 00:54:19 +0200

    Apache Spark

--- scripts/test-sandbox.sh
+++ scripts/test-sandbox.sh
@@ -2,5 +2,23 @@
 
 hdfs dfs -ls /user
 
+echo "Maven"
 cd /home/sages/repository/HADOOP
 mvn clean install -Puber,sandbox
+
+
+echo "MapReduce"
+hdfs dfs -rm -f -r -skipTrash $HADOOP_HDFS_HOME/dane
+hdfs dfs -rm -f -r -skipTrash $HADOOP_HDFS_HOME/wyniki
+
+yarn jar $HADOOP_PROJECT/mapreduce/target/mapreduce-1.0-SNAPSHOT-jar-with-dependencies.jar pl.com.sages.hadoop.mapreduce.wordcount.WordCountRunner /dane/lektury/lektury-one-file $HADOOP_HDFS_HOME/wyniki/lektury-one-file-wordcount-output
+yarn jar $HADOOP_PROJECT/mapreduce/target/mapreduce-1.0-SNAPSHOT-jar-with-dependencies.jar pl.com.sages.hadoop.mapreduce.invertedindex.InvertedIndexRunner /dane/lektury/lektury-one-file $HADOOP_HDFS_HOME/wyniki/lektury-one-file-invertedindex-output
+
+
+echo "Hive"
+
+
+echo "Pig"
+
+
+echo "Spark"
