24401b38ef61bab10108283843a8d8b2e063106b
Radosław Szmit <radoslawszmit@gmail.com>
2019-11-26 22:26:14 +0100

    Refaktoryzacja

--- spark/spark-sql/src/main/scala/pl/com/sages/spark/sql/model/Movie.scala
+++
@@ -1,5 +0,0 @@
-package pl.com.sages.spark.sql.model
-
-case class Movie(movieId: String, title: String, genres: String){
-
-}
--- spark/spark-sql/src/main/scala/pl/com/sages/spark/sql/model/Rating.scala
+++
@@ -1,5 +0,0 @@
-package pl.com.sages.spark.sql.model
-
-case class Rating(userId: String, movieId: String, rating: String, timestamp: String) {
-
-}
